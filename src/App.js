512bc26b8f35b79a7e1036e6eb9a03e3c64a5c1b
Toshiaki Furukawa <furukawa@serverworks.co.jp>
2020-07-01 01:15:38 +0900

    cognitoによりログイン、サインアップ機能の追加

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Amplify from '@aws-amplify/core';
+import awsmobile from './aws-exports';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsmobile);
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>
+      Hello World!
+    </h1>
   );
 }
 
-export default App;
+export default withAuthenticator(App, {
+  signUpConfig: {
+    hiddenDefaults: ['phone_number']
+  }
+});
